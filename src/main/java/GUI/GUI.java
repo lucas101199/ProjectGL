42dbfb59b8366a8ae81bc75ae081dbf602e635a2
lucas <lucas_fernandez@orange.fr>
2020-09-22 18:04:35 +0200

    add screen for print the floor

--- src/main/java/GUI/GUI.java
+++ src/main/java/GUI/GUI.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class GUI extends Frame {
 
     public JFrame frame;
-    public JPanel button_group;
+    public JPanel button_group, screen;
     public GridBagConstraints gbc_button;
     public ArrayList<JButton> button_floor;
     public JButton emergency;
@@ -23,20 +23,27 @@ public class GUI extends Frame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 1200);
         frame.setLocationRelativeTo(null);
-        frame.setLayout(new BorderLayout());
+        frame.setLayout(new GridBagLayout());
 
         gbc_button = new GridBagConstraints();
         button_group = new JPanel();
         button_group.setLayout(new GridBagLayout());
-        button_group.setBorder(blackLine);
-        button_group.setSize(300, 1000);
+        //button_group.setBorder(blackLine);
+        button_group.setSize(300, 800);
         gbc_button.gridwidth = GridBagConstraints.REMAINDER;
 
         //emergency stop button
         emergency = new JButton("stop");
         button_group.add(emergency, gbc_button);
 
-        frame.getContentPane().add(button_group, BorderLayout.CENTER);
+        //Screen with the floor
+        screen = new JPanel();
+        screen.setBorder(blackLine);
+        screen.setSize(300, 200);
+
+        //add all the components to the frame
+        frame.getContentPane().add(screen, gbc_button);
+        frame.getContentPane().add(button_group, gbc_button);
         frame.setVisible(true);
     }
 
