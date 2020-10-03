b42d7d0bffb3f8824965f43c2f3ba62195eb018e
lucas <lucas_fernandez@orange.fr>
2020-10-03 17:25:04 +0200

    GUI to GUI_CC in Gui_elevator

--- src/main/java/GUI/Gui_elevator.java
+++ src/main/java/GUI/Gui_elevator.java
@@ -4,11 +4,11 @@ public class Gui_elevator {
 
     public static void main(String[] args) {
         try {
-            GUI elevator = new GUI();
-            elevator.add_Button("0");
-            elevator.add_Button("1");
-            elevator.add_Button("2");
-            elevator.add_Button("3");
+            GUI_CC elevator = new GUI_CC();
+            elevator.gui.add_Button("0");
+            elevator.gui.add_Button("1");
+            elevator.gui.add_Button("2");
+            elevator.gui.add_Button("3");
         } catch(Exception e) {
             e.printStackTrace();
         }
